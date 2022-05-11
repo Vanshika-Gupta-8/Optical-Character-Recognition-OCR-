e5d339c1fb7b6bbd49fbbbecb7f5f205aa5d93d3
Vanshika Gupta <84127620+Vanshika-Gupta-8@users.noreply.github.com>
2022-05-11 20:08:55 +0530

    Delete Training directory

--- Training/Training/randwt.m
+++
@@ -1,8 +0,0 @@
-function [theta] = randwt(r, c)
-%% Randomly initialize theta with some weights so as to break symmetry problem
-
-INIT_EPSILON = sqrt(6)/sqrt(r+c);
-theta = rand(r,c) * (2*INIT_EPSILON) - INIT_EPSILON;
-
-end
-
--- Training/Training/siggrad.m
+++
@@ -1,8 +0,0 @@
-function [g] = siggrad(z)
-%% Calculate the gradient of sigmoid function for a vector or matrix or scalar
-
-h = sigmoid(z);
-g = h .* (1 - h);
-
-end
-
